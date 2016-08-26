f1d0419cd85b9535b9702ffed70fba7959cdf19d
DavidKleinberg <davokleinberg@gmail.com>
2016-08-25 23:33:50 -0400

    Update FileGenerator.java

--- FileGenerator.java
+++ FileGenerator.java
@@ -6,14 +6,16 @@ import java.io.PrintWriter;
 
 public class FileGenerator {
 
-   public FileGenerator(String outputFile, GraphData originalData, 
-   PeakFinder peaks) throws IOException {
+   //public FileGenerator(String outputFile, GraphData originalData, 
+   //PeakFinder peaks) throws IOException {
+   public FileGenerator(String outputFile, Stock stock) throws IOException {
 
    PrintWriter outFile = new PrintWriter(new FileWriter(outputFile));
-         outFile.println(originalData.type);
-         outFile.print(originalData + "\n");
-         outFile.println(peaks.type);
-         outFile.print(peaks + "\n");
+         //outFile.println(originalData.type);
+         //outFile.print(originalData + "\n");
+         //outFile.println(peaks.type);
+         //outFile.print(peaks + "\n");
+         outFile.print(stock);
          
          outFile.close();
 
